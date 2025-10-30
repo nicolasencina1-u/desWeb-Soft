98c87a234986eebd551bb7932979af57658bdcc5
nicolasencina1-u <nicolas.encina1@mail.udp.cl>
2025-10-30 17:19:45 -0300

    Update roulette.js

--- public/js/roulette.js
+++ public/js/roulette.js
@@ -194,16 +194,16 @@ document.addEventListener('DOMContentLoaded', () => {
       const slotAngle = 360 / wheelNumbersOrder.length;
       
       // 2. Calcula el ángulo de detención (centrado en el slot)
-      const targetAngle = -(index * slotAngle);
+      const targetAngle = (index * slotAngle);
       
       // 3. CÁLCULO DE LA RULETA (IMAGEN)
       // 5 rotaciones (sentido horario) + la posición final ANTI-HORARIA (-targetAngle)
-      const wheelRotation = targetAngle;
+      const wheelRotation = (360 * 8) + targetAngle;
 
       // 4. CÁLCULO DE LA BOLA
       // 8 rotaciones (sentido anti-horario) + la misma posición final ANTI-HORARIA (-targetAngle)
       // La bola gira en dirección opuesta, pero ambas deben aterrizar en el mismo ángulo.
-      const ballRotation = targetAngle + (slotAngle / 2);
+      const ballRotation = (360 * 8) + (slotAngle / 2);
 
       // 5. ANIMACIÓN
       
